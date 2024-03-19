8548b292d25281cf12ed51e43e91986362e7dc39
JoaquinLaGringa <164096606+JoaquinLaGringa@users.noreply.github.com>
2024-03-19 19:26:15 -0300

    Add files via upload

---
+++ programa1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int a[] = {1,2,3,4,5};
+	int sumapar=0, sumaimpar=0;
+	for(int = 0;i<5;i++){
+		int num=a[i];
+		if(num%2==0){
+			(sumapar+=num);
+			else {
+			sumaimpar+=0;
+			}
+		}
+		
+	};
+printf("sp:%d","si:%d",sumapar,sumaimpar);
+	
+	return 0;
+}
+
